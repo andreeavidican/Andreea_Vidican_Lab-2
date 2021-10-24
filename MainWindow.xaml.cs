e330f28dd43b61225f813145073a9674189a804c
Andreea Vidican <vidicanandreea1@gmail.com>
2021-10-24 14:05:20 +0300

    schimbari

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,55 @@ using System.Windows.Shapes;
 
 namespace Andreea_Vidican_Lab_2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+        private int mRaisedGlazed;
+        private int mRaisedSugar;
+        private int mFilledLemon;
+        private int mFilledChocolate;
+        private int mFilledVanilla;
+
+        private DoughnutMachine myDoughnutMachine;
+
         public MainWindow()
         {
+
             InitializeComponent();
         }
+
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+        
+        }
+        private void glazedToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedToolStripMenuItem.IsChecked = true;
+            sugarToolStripMenuItem.IsChecked = false;
+            myDoughnutMachine.MakeDoughnuts(DoughnutType.Glazed);
+        }
+        private void sugarToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+        private void DoughnutCompleteHandler()
+        {
+            switch (myDoughnutMachine.Flavor)
+            {
+                case DoughnutType.Glazed:
+                    mRaisedGlazed++;
+                    txtGlazedRaised.Text = mRaisedGlazed.ToString();
+                    break;
+
+                case DoughnutType.Sugar:
+                    mRaisedSugar++;
+                    txtSugarRaised.Text = mRaisedSugar.ToString();
+                    break;
+                    
+            }
+        }
+
     }
+
+    
 }
